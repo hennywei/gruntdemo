ac65255ec93ec8a841b0a21c3b48e919573feac7
hennywei <hennywei@gmail.com>
2015-02-02 22:35:05 +0800

    first grunt jquery plugin

---
+++ README.md
@@ -0,0 +1,30 @@
+# gruntdemo
+
+first grunt jquery plugin
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/hennywei/gruntdemo/master/dist/gruntdemo.min.js
+[max]: https://raw.github.com/hennywei/gruntdemo/master/dist/gruntdemo.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/gruntdemo.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery.gruntdemo.js
@@ -0,0 +1,38 @@
+/*
+ * gruntdemo
+ * https://github.com/hennywei/gruntdemo
+ *
+ * Copyright (c) 2015 hennywei
+ * Licensed under the GPL license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.gruntdemo = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.gruntdemo = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.gruntdemo.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.gruntdemo.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].gruntdemo = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
